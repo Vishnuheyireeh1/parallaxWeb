4b5dbf9bb88b55763b1113036da47d7b6717344c
vishnuheyireeh1 <blakzz456@gmail.com>
2024-10-17 11:41:46 +0530

    parallaxWeb completed

---
+++ src/Text.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">lorem</h1>
+                <p id="textblock-content">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quisquam maiores, quos dolores autem ad dolor consequatur reprehenderit repellendus repudiandae dignissimos numquam at animi, similique neque accusantium blanditiis, quas voluptatem.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default TextBlock;
